e42721586043797bcdde1952f31a502b8eaf8c7a
ElieSun <elinavoloshyk@gmail.com>
2022-11-08 14:57:42 +0200

    hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,10 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import Player from '@vimeo/player';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log('Hello word');
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import {save, load,} from "./localStorage.js"
 
+    import Player from '@vimeo/player';
+    var throttle = require('lodash.throttle');
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    player.setCurrentTime(load("videoplayer-current-time")).then(function(seconds) {
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+            default:
+                break;
+        }
+    });
+    const onTimeUpdate = function(data){
+        save("videoplayer-current-time", data.seconds)
+    }
+    player.on('timeupdate', throttle(onTimeUpdate, 1000));
