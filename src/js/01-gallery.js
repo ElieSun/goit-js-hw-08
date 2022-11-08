66d91578f572f58c41d2ebee23b0927d7b5f40ea
ElieSun <elinavoloshyk@gmail.com>
2022-11-08 12:19:16 +0200

    hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,11 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import Player from '@vimeo/player';
+console.log(Player);
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 console.log('Hello word');
+
