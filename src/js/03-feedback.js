5e866d50546ba3c0fbd97d3f80510e4c16a4a59c
ElieSun <elinavoloshyk@gmail.com>
2022-11-15 15:56:46 +0200

    last-v

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,35 @@
-import throttle from 'lodash.throttle';
+    import throttle from 'lodash.throttle';
 
     const refs = {
         form: document.querySelector('.feedback-form'),
     };
-        const STORAGE_KEY = 'form-data';
+    
+    const STORAGE_KEY = 'form-data';
 
     updateFormFields();
+    
+    function updateFormFields () {
+        const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        Object.entries(data || { email: '', message: '' }).forEach(([name,value]) => {
+            refs.form.elements[name].value = value;
+        })
+    };
 
-        refs.form.addEventListener('input', throttle(handleInput, 500));
-        refs.form.addEventListener('submit', handleSubmit);
-
-        function handleSubmit(event) {
-            event.preventDfault();
-            const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-            if(!data.message || !data.email) return;
-            console.log(data);
-            localStorage.removeItem(STORAGE_KEY);
-            updateFormFields ();
-        };
+    refs.form.addEventListener('input', throttle(handleInput, 500));
 
     function handleInput({target:{ name, value } }) { 
         const store = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
         store[name] = value;
         localStorage.setItem(STORAGE_KEY, JSON.stringify(store));
     }
-    
-        function updateFormFields () {
-            const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-            Object.entries(data || { email: '', message: '' }).forEach(([name,value]) => {
-                refs.form.elements[name].value = value;
-            })
-        };
+
+    refs.form.addEventListener('submit', handleSubmit);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        if(!data.message || !data.email) return;
+        console.log(data);
+        localStorage.removeItem(STORAGE_KEY);
+        updateFormFields ();
+    };
